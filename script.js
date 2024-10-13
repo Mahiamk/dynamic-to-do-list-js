899f9a434baa473b09ce34643cd5258571987cd4
Anwar Koji <camk0966@gmail.com>
2024-10-14 04:41:59 +0800

    Done

---
+++ script.js
@@ -0,0 +1,36 @@
+document.addEventListener("DOMContentLoaded", function(){
+  const addButton = document.getElementById("add-task-btn");
+  const taskInput = document.getElementById("task-input");
+  const taskList = document.getElementById("task-list");
+
+  function addTask(){
+    const taskText = taskInput.ariaValueMax.trim();
+    if (taskText === ""){
+      alert("Please enter a task.");
+      retuern;
+    }
+
+    const listItem = document.getElementById("li");
+    listItem.textContent=taskText;
+
+    const removeButton = document.createElement("button");
+    removeButton.textContent = "Remove";
+    removeButton.className="remove-btn";
+    removeButton.onclick = function (){
+      taskList.removeChild(listItem);
+    };
+
+    listItem.appendChild(removeButton);
+    taskList.appendChild(listItem);
+    taskInput.value = "";
+
+  }
+  addButton.addEventListener("click", addTask);
+  taskInput.addEventListener("keypress", function(event){
+    if (event.key == "Enter"){
+      addTask();
+    }
+  });
+
+
+});
